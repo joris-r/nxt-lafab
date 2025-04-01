2592c267147153395a926f4446a56d79f7e8b84d
Joris Rehm <joris@jirm.eu>
2025-04-01 22:28:48 +0200

    cleaning and dir config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,29 @@
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
-export default async function(eleventyConfig) {
-  // Configure Eleventy
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+export const config = {
+	// These are all optional:
+	dir: {
+		input: "content",          // default: "."
+		includes: "../_includes",  // default: "_includes" (`input` relative)
+		data: "../_data",          // default: "_data" (`input` relative)
+		output: "_site"
+	},
+
+	// Pre-process *.md files with: (default: `liquid`)
+	markdownTemplateEngine: "njk",
+
+	// Pre-process *.html files with: (default: `liquid`)
+	htmlTemplateEngine: "njk",
 
 };
+
+export default async function(eleventyConfig) {
+
+  // Copy the contents of the `public` folder to the output folder
+	// For example, `./public/css/` ends up in `_site/css/`
+	eleventyConfig
+  .addPassthroughCopy({
+    "./public/": "/"
+  })
+  
+};
+
